2a43db816b022b9309b850a114d869c9a204f24f
Alberto Cienfuegos Álvarez <UO308387@uniovi.es>
2026-02-04 19:53:59 +0100

    Cambios clase

--- p11/rutalaboratorio.bat
+++ p11/rutalaboratorio.bat
@@ -1,2 +1,2 @@
-set path=C:\Program Files\Java\<jdk>\bin
-set classpath=C:\Users\<loquesea>\<repositorio github>\p11
+set path=C:\Program Files\Java\jdk-24\bin
+set classpath=C:\Users\UO308387\Desktop\alg_CienfuegosAlvarezAlbertoUO308387\p11
