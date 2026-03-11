266d762a44538c7b76e7e8fbeb92b973cb9bd609
Alberto Cienfuegos Álvarez <UO308387@uniovi.es>
2026-03-11 19:59:17 +0100

    Clase p4

---
+++ p4/proyectoJava/src/p4/ColoreoGrafo.java
@@ -0,0 +1,24 @@
+package p4;
+public class ColoreoGrafo {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+
+
+        /**
+         * Definir la estructura de datos para guardar la representacion del mapa
+         * y la paleta de colores
+         *
+         * realizarVoraz devuelve un map de string string
+         * reccorrer los nodos, para cada uno de los nodos visitamos sus vecinos
+         * si algun vecino ya tiene algun color del diccionario no esta disponible
+         * para ser usado (los numero de dentro de la lista del json son los vecinos del nodo)
+         * 
+         * Una vez identificamos los vecimos y sus colores, seleccionamos el color que no tiene asignado ningun
+         * vecino, y asi todo el rato
+         * 
+         * HACER QUE EL ALGORITMO USE EL MENOR NUMERO DE COLORES 
+         *
+         */
+    }
+}
